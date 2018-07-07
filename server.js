c8a253c723d1bb57d4a7475c375883634aa29685
LunaPC <lunad3@uci.edu>
2018-07-06 17:44:26 -0700

    correctly grab article tiles, but not correct information, need to fine tune child selector for element

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const cheerio = require("cheerio");
 const request = require("request");
 const website = "https://www.nationalgeographic.org/news/";
 
+console.log("SERVER RUNNING");
 // grab the html from the webpage
 request(website,(error,response,html)=>{
-
+    console.log("INSIDE REQUEST");
     // use cheerio to mimic jquery/ajax
     const $ = cheerio.load(html);
-    let results = [];
+    let articles = [];
 
     // select element of interest to grab information from
-    $(".ng-margin-top .ng-clearfix").each((i,element)=>{
-
+    $(".ng-search-result-tile").each((i,element)=>{
+        console.log("articles ++");
         // grab specific information from the webpage
-        // const headline = $(element).children().text();
-        // const summary = $().text();
-        // const url = website + $(element).attr("href");
-
+        const article = {
+            url: website + $(element).children(".ng-promo-title a").attr("href"),
+            thumbnail: website + $(element).children(".ng-image-link").attr("href"),
+            headline: $(element).children(".ng-promo-title a").text(),
+            summary: $(element).children("p").text()
+        }
         // place information inside an Obj and add to results arr
-        // results.push({
-        //     headline: headline,
-            // summary: summary,
-            // url: url
-        // });
+        articles.push(article);
     });
+
+    for(let i=0; i<articles.length; i++){
+        console.log()
+        console.log(articles[i]);
+    }
 });
